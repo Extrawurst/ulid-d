9819e35cd5d2ac694b15a7e8047951487999081b
Stephan Dilly <dilly.stephan@gmail.com>
2017-10-26 12:55:17 +0200

    add demo and more docs on readme.md

--- source/demo.d
+++ source/demo.d
@@ -1,8 +1,5 @@
-# ulid-d
-ULID implementation in D
+module demo;
 
-# usage
-```
 import std.stdio;
 
 import ulid.ulid;
@@ -27,24 +24,3 @@ void main()
     writefln("all custom: %s", ULID.generate(1469918176385, &randomByte).toString());
     writefln("all custom: %s", ULID.generate(1469918176385, &randomByte).toString());
 }
-```
-
-see `source/demo.d`
-
-# build demo
-
-```
-dub --config=demo
-```
-
-output:
-
-```
-simple: 7DFPGD9GPH2X5T9Y05S7EDW3N2
-simple: 7DFPGDEC1JQC153KSP6VPEF27T
-custom time: 01ARYZ6S410EHN4S8FDPCWWQ00
-custom time: 01ARYZ6S413XBZ5V68XJJTV5FS
-custom time: 01ARYZ6S41 <- time portion
-all custom: 01ARYZ6S410G2081040G208104
-all custom: 01ARYZ6S410G2081040G208104
-```
